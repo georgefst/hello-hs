a2e4d4a8ac386f116eb63d3d1017d8ef1db707dd
George Thomas <georgefsthomas@gmail.com>
2022-08-10 11:35:19 +0100

    Add support for building _from_ M1 Mac
    I haven't fully utilised this yet as there are things it has to build (e.g. `aarch64-unknown-linux-gnu-binutils-2.38` is not in cache), but it _should_ work.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
       let
         overlays = [
           haskellNix.overlay
