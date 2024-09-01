f3a274f62abf6933477f945ef864c215265630c3
George Thomas <georgefsthomas@gmail.com>
2024-09-01 12:32:43 +0100

    cabal-cpp-ghc-version-bug

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+#if __GLASGOW_HASKELL__ >= 908
+-- Cabal 3.10 hits here
+main = putStrLn "9.8 (or higher)"
+#elif __GLASGOW_HASKELL__ >= 906
+main = putStrLn "9.6"
+#elif __GLASGOW_HASKELL__ >= 904
+main = putStrLn "9.4"
+#elif __GLASGOW_HASKELL__ >= 902
+-- Cabal 3.12 (on Billy) erroneously hits here, even with system GHC renamed
+main = putStrLn "9.2"
+#else
+main = putStrLn "unknown"
+#endif
