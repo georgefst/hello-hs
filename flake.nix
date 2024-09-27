70cb061ac50f912138a0b35da0a92025441ad9ee
George Thomas <georgefsthomas@gmail.com>
2024-09-27 12:58:08 +0100

    Regenerate Nix setup based on latest upstream docs
    I started hitting various weird errors, recompiling GHC etc. so it seemed a good time to switch to the more modern approach.
    
    Refactoring has been performed in order to reduce the diff about as much as possible. In particular, Hix seems to be designed to auto-pickup everything in `final.haskell-nix.hix.project` config from an external `hix.nix` file, but we've kept it all in one file instead.
    
    We've taken some inessential suggestions from the template: including more cross-compilation targets, and adding dev tools to the shell. On the other hand we've disregarded the suggestion to include `legacyPackages`, which doesn't make any noticeable difference.

--- flake.nix
+++ flake.nix
@@ -7,23 +7,32 @@
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
       let
-        crossPlatforms = p: [
-          # Provides `nix build .#aarch64-unknown-linux-gnu:hello-hs:exe:hello-hs`.
-          # And `aarch64-unknown-linux-gnu-cabal` etc. in the shell.
-          p.aarch64-multiplatform
-        ];
         overlays = [
           haskellNix.overlay
           (final: prev: {
             myHaskellProject =
-              final.haskell-nix.project' {
+              final.haskell-nix.hix.project {
                 src = ./.;
-                compiler-nix-name = "ghc924";
-                shell = { inherit crossPlatforms; };
+                compiler-nix-name = "ghc92";
+                evalSystem = "x86_64-linux";
+                crossPlatforms = p:
+                  pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64
+                    ([
+                      p.mingwW64
+                      p.ghcjs
+                    ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux
+                      [
+                        p.musl64
+                        p.aarch64-multiplatform
+                      ]
+                    );
+                shell.tools.cabal = "latest";
+                shell.tools.hlint = "latest";
+                shell.tools.haskell-language-server = "latest";
               };
           })
         ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
       in
-      pkgs.myHaskellProject.flake { inherit crossPlatforms; });
+      pkgs.myHaskellProject.flake { });
 }
