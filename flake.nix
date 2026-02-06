8608c53931b0400a4985eecd4fde3159eaafe35e
George Thomas <georgefsthomas@gmail.com>
2026-02-06 22:45:16 +0000

    Use patched GHC 9.12.3
    This is all for Wasm support.
    Since Stable Haskell GHC 9.12.2 missed some important Wasm backend changes for Miso, such as synchronous JS FFI, and 9.12.4 isn't out yet, we use 9.12.3 with patches for:
    - The critical code gen issue with sub-word divisions which resulted in 9.12.3 being generally unsupported.
    - Multi-line JS FFI, which is used by Miso, and wouldn't otherwise be available until 9.14.
    
    Note that this means none of our GHCs are in IOG caches. We have to build the Wasm cross-compiler ourselves, which takes about 20 minutes on decent hardware. And we disable all other cross platforms for now, so we don't have to wait for them to build too.
    
    Note that due to some weird, possibly FOD-related, network restriction, we had to manually add the source to the store before building: `nix eval --expr 'builtins.fetchGit { url = "https://github.com/stable-haskell/ghc"; rev = "8b63dfe0a81d08fef5b4acba6e60d80067daa5d7"; name = "ghc-8b63dfe"; submodules = true; }'`.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Basic Haskell flake";
-  inputs.haskell-nix.url = "github:input-output-hk/haskell.nix";
+  inputs.haskell-nix.url = "github:georgefst/haskell.nix/wasm-9.12.3";
   inputs.nixpkgs.follows = "haskell-nix/nixpkgs-2511";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = inputs@{ self, nixpkgs, flake-utils, haskell-nix, ... }:
@@ -12,19 +12,19 @@
             myHaskellProject =
               final.haskell-nix.hix.project {
                 src = ./.;
-                compiler-nix-name = "ghc9122";
+                compiler-nix-name = "ghc9123";
                 evalSystem = "x86_64-linux";
                 crossPlatforms = p:
                   pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64
                     ([
-                      p.ghcjs
+                      # p.ghcjs
                       p.wasi32
                       # TODO not currently available in caches, and takes a long time to build from source
                       # p.mingwW64
                     ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux
                       [
-                        p.musl64
-                        p.aarch64-multiplatform
+                        # p.musl64
+                        # p.aarch64-multiplatform
                       ]
                     );
                 shell.tools.cabal = "latest";
